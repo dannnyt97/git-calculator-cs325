9059afd628b8f3a2ef4694c8e5218e1dc46e5b55
Allison M <aidandmc7@gmail.com>
2026-09-08 20:34:45 -0500

    feat: add divide function

--- calculator.py
+++ calculator.py
@@ -7,11 +7,15 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b
+
 def calculate():
     print("Welcome to the Pair Calculator!")
     print("Addition: 5 + 3 =", add(5, 3))
     print("Subtraction: 5 - 3 =", subtract(5, 3))
     print("Multiplication: 5 * 3 =", multiply(5, 3))
+    print("Divide: 5 * 3 =", divide(5, 3))
 
 if __name__ == "__main__":
     calculate()
