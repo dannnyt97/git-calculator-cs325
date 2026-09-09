52ca8f7150b4b7c2eba73cbf8eca5f8c82192702
Allison M <aidandmc7@gmail.com>
2026-09-08 20:58:57 -0500

    chore: update welcome banner B

--- calculator.py
+++ calculator.py
@@ -14,7 +14,7 @@ def divide(a, b):
         return a / b
 
 def calculate():
-    print("Welcome to the Pair Calculator!")
+    print(">>> Super Calculator: Version B <<<")
     print("Addition: 5 + 3 =", add(5, 3))
     print("Subtraction: 5 - 3 =", subtract(5, 3))
     print("Multiplication: 5 * 3 =", multiply(5, 3))
